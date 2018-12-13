fa7808d8553456db8f57ebbc14f9f4e624d5871b
cmoutafidis <charismoutafidis@gmail.com>
2018-12-13 12:11:00 +0200

    Added raindrop sensor, bmp180 is not working.

--- weatherstationarduino.ino
+++ weatherstationarduino.ino
@@ -1,15 +1,26 @@
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#define ALTITUDE 1655.0
+
 #include <SoftwareSerial.h>
 SoftwareSerial espSerial = SoftwareSerial(2, 3);
 
 #include <DHT.h> 
 #define DHTPIN 5
+#define DHTPIN2 6
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
+DHT dht2(DHTPIN2, DHTTYPE);
+
+SFE_BMP180 pressure;
 
 String apiKey = "";
 
 String ssid = "";
 String password = "";
+
+const int sensorMin = 0;
+const int sensorMax = 1024;
 
 long time1;
 long time2;
@@ -23,7 +34,6 @@ void showResponse(int waitTime) {
             Serial.print(c);
         }
     }
-
 }
 
 boolean thingSpeakWrite(float value1, float value2) {
@@ -37,7 +47,7 @@ boolean thingSpeakWrite(float value1, float value2) {
         return false;
     }
 
-    String getStr = "GET /update?api_key="; // prepare GET string
+    String getStr = "GET /update?api_key=";
     getStr += apiKey;
 
     getStr += "&field1=";
@@ -57,7 +67,6 @@ boolean thingSpeakWrite(float value1, float value2) {
         Serial.print(getStr);
     } else {
         espSerial.println("AT+CIPCLOSE");
-        // alert user
         Serial.println("AT+CIPCLOSE");
         return false;
     }
@@ -66,8 +75,19 @@ boolean thingSpeakWrite(float value1, float value2) {
 
 void setup() {
     Serial.begin(9600);
+    if (pressure.begin()){
+      Serial.println("BMP180 init success");
+    }
+    else{
+      // Oops, something went wrong, this is usually a connection problem,
+      // see the comments at the top of this sketch for the proper connections.
+  
+      Serial.println("BMP180 init fail\n\n");
+      while(1); // Pause forever.
+    }
 
     dht.begin();
+    dht2.begin();
 
     espSerial.begin(9600);
     espSerial.println("AT+CWMODE=1");
@@ -78,11 +98,24 @@ void setup() {
 
     Serial.println("Setup completed");
 
-    time1 = millis();
+    time1 = 0;
 }
 
 void loop() {
-
+    int sensorReading = analogRead(A0);
+    int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+    switch (range) {
+    case 0:    // Sensor getting wet
+        Serial.println("Flood");
+        break;
+    case 1:    // Sensor getting wet
+        Serial.println("Rain Warning");
+        break;
+    case 2:    // Sensor dry - To shut this up delete the " Serial.println("Not Raining"); " below.
+        Serial.println("Not Raining");
+        break;
+    }
+  
     float t = dht.readTemperature();
     float h = dht.readHumidity();
     if (isnan(t) || isnan(h)) {
@@ -90,6 +123,15 @@ void loop() {
     } else {
         Serial.println("Temperature= " + String(t) + " *C");
         Serial.println("Humidity= " + String(h) + " %");
+    }
+
+    float t2 = dht.readTemperature();
+    float h2 = dht.readHumidity();
+    if (isnan(t2) || isnan(h2)) {
+        Serial.println("Failed to read from DHT2");
+    } else {
+        Serial.println("Temperature2= " + String(t2) + " *C");
+        Serial.println("Humidity2= " + String(h2) + " %");
     }
 
     time2 = millis();
